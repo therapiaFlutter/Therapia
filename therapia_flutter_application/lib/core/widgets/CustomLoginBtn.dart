ebba036b98e8ebba287d327ab356a83c072cf3a8
Youssef Souissi <souissiyoussef9@gmail.com>
2023-12-27 05:08:30 +0100

    create customLoginBtn widget

---
+++ therapia_flutter_application/lib/core/widgets/CustomLoginBtn.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CustomButtonStyle extends StatelessWidget {
+ final Function()? onTap;
+ final String btnText;
+
+ const CustomButtonStyle({Key? key, required this.onTap, required this.btnText}) : super(key: key);
+
+ @override
+ Widget build(BuildContext context) {
+   return GestureDetector(
+     onTap: onTap,
+     child: Container(
+       
+       margin: const EdgeInsets.symmetric(horizontal: 35),
+       decoration: BoxDecoration(
+         color: Color(0xFF734BFB),
+         borderRadius: BorderRadius.circular(48),
+         
+       ),
+       height: 70,
+       width: 300,
+       child: Center(
+         child: Text(
+           btnText,
+           style: TextStyle(
+             color: Color(0xFFF0EFF6),
+             fontFamily: 'Quicksand', fontWeight: FontWeight.w600,
+             fontSize: 22,
+             letterSpacing: 1,
+           ),
+         ),
+       ),
+     ),
+   );
+ }
+}
