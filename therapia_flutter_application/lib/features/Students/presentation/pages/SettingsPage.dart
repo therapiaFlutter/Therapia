c7737db87794c134a1bbb3bb6b832f4cd1e70bdd
Youssef Souissi <souissiyoussef9@gmail.com>
2023-12-29 06:24:27 +0100

    navigation between settings page and profile

--- therapia_flutter_application/lib/features/Students/presentation/pages/SettingsPage.dart
+++ therapia_flutter_application/lib/features/Students/presentation/pages/SettingsPage.dart
@@ -1,93 +1,132 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:therapia_flutter_application/core/colors/PageBackground.dart';
-import 'package:therapia_flutter_application/features/auth/presentation/bloc/auth/auth_bloc.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-
-
+import 'package:therapia_flutter_application/core/colors/PageBackground.dart';
+import 'package:therapia_flutter_application/features/Students/presentation/pages/Profile.dart';
+import 'package:therapia_flutter_application/features/auth/presentation/bloc/auth/auth_bloc.dart';
 
 class Settings extends StatefulWidget {
- const Settings({Key? key}) : super(key: key);
+  const Settings({Key? key}) : super(key: key);
 
- @override
- State<Settings> createState() => _SettingsState();
+  @override
+  State<Settings> createState() => _SettingsState();
 }
 
 class _SettingsState extends State<Settings> {
- @override
- Widget build(BuildContext context) {
-  return Scaffold(
-
-    body: Center(
-      child: Container(
-        decoration: BoxDecoration(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
             gradient: PageBackground.getBackgroundColor(),
           ),
-        constraints: const BoxConstraints(maxWidth: 400),
-        child: ListView(
-          children: [
-            _SingleSection(
-              title: "General",
-              children: [
-                const _CustomListTile(
-                  title: "Notifications",
-                  icon: Icons.notifications_none_rounded,
-                ),
-              ],
-            ),
-            const Divider(),
-            const _SingleSection(
-              title: "Organization",
-              children: [
-                _CustomListTile(
-                  title: "Profile",
-                  icon: Icons.person_outline_rounded,
-                ),
-                _CustomListTile(
-                  title: "My Appointments",
-                  icon: Icons.calendar_today_rounded,
-                ),
-              ],
-            ),
-            const Divider(),
-            const _SingleSection(
-              children: [
-                _CustomListTile(
-                  title: "Help & Feedback",
-                  icon: Icons.help_outline_rounded,
-                ),
-                _CustomListTile(
-                  title: "About",
-                  icon: Icons.info_outline_rounded,
-                ),
-                _CustomListTile(
-                  title: "Sign out",
-                
-                  icon: Icons.exit_to_app_rounded,
-                ),
-              ],
-            ),
-          ],
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: ListView(
+            children: [
+              _SingleSection(
+                title: "General",
+                children: const [
+                  _CustomListTile(
+                    title: "Notifications",
+                    icon: Icons.notifications_none_rounded,
+                  ),
+                ],
+              ),
+              const Divider(),
+              _SingleSection(
+                title: "Organization",
+                children: [
+                  _CustomListTile(
+                    title: "Profile",
+                    icon: Icons.person_outline_rounded,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProfilePage(),
+                        ),
+                      );
+                    },
+                  ),
+                  _CustomListTile(
+                    title: "My Appointments",
+                    icon: Icons.calendar_today_rounded,
+                  ),
+                ],
+              ),
+              const Divider(),
+              _SingleSection(
+                children: [
+                  _CustomListTile(
+                    title: "Help & Feedback",
+                    icon: Icons.help_outline_rounded,
+                  ),
+                  _CustomListTile(
+                    title: "About",
+                    icon: Icons.info_outline_rounded,
+                  ),
+                  _CustomListTile(
+                    title: "Sign out",
+                    icon: Icons.exit_to_app_rounded,
+                    onTap: () {
+                      _showLogoutDialog(context);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
- }
+    );
+  }
+
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Logout"),
+          content: const Text("Are you sure you want to logout?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                // Perform actual logout action here
+                // For example, you can dispatch a logout event to your authentication bloc
+                BlocProvider.of<AuthBloc>(context).add(LogoutEvent());
+                // Use GoRouter to navigate to the login page
+                GoRouter.of(context).go('/login');
+
+                Navigator.pop(context); // Close the dialog
+              },
+              child: const Text("Logout"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
 
 class _CustomListTile extends StatelessWidget {
   final String title;
   final IconData icon;
-  final Widget? trailing;
+  final VoidCallback? onTap;
 
   const _CustomListTile({
     Key? key,
     required this.title,
     required this.icon,
-    this.trailing,
+    this.onTap,
   }) : super(key: key);
 
   @override
@@ -95,88 +134,50 @@ class _CustomListTile extends StatelessWidget {
     return ListTile(
       title: Text(
         title,
-        style: TextStyle(
+        style: const TextStyle(
           fontFamily: 'Quicksand',
           fontWeight: FontWeight.w600,
         ),
       ),
       leading: Icon(icon),
-      trailing: trailing,
-      onTap: () {
-        // Perform logout action here
-        _showLogoutDialog(context);
-      },
+      onTap: onTap,
     );
   }
-
-void _showLogoutDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text("Logout"),
-        content: Text("Are you sure you want to logout?"),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context); // Close the dialog
-            },
-            child: Text("Cancel"),
-          ),
-          TextButton(
-            onPressed: () {
-              // Perform actual logout action here
-              // For example, you can dispatch a logout event to your authentication bloc
-              // AuthBlocProvider.of(context).logout();
-             BlocProvider.of<AuthBloc>(context).add(LogoutEvent());
-            // Use GoRouter to navigate to the login page
-              GoRouter.of(context).go('/login');
-
-              Navigator.pop(context); // Close the dialog
-            },
-            child: Text("Logout"),
-          ),
-        ],
-      );
-    },
-  );
-}
-
-
 }
 
 class _SingleSection extends StatelessWidget {
- final String? title;
- final List<Widget> children;
- const _SingleSection({
-  Key? key,
-  this.title,
-  required this.children,
- }) : super(key: key);
+  final String? title;
+  final List<Widget> children;
 
- @override
- Widget build(BuildContext context) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      if (title != null)
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            title!,
-            style: const TextStyle(
-              fontSize: 17,
-              fontFamily: 'Quicksand',
-              fontWeight: FontWeight.w800,
-              color: Color(0xFF734BFB)
+  const _SingleSection({
+    Key? key,
+    this.title,
+    required this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (title != null)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              title!,
+              style: const TextStyle(
+                fontSize: 17,
+                fontFamily: 'Quicksand',
+                fontWeight: FontWeight.w800,
+                color: Color(0xFF734BFB),
+              ),
             ),
           ),
+        Column(
+          children: children,
         ),
-      Column(
-        children: children,
-      ),
-    ],
-  );
- }
+      ],
+    );
+  }
 }
