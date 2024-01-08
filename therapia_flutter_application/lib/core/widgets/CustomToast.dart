874aee3f32c25fbf58cbf3c658cdf324a4714d0c
Youssef Souissi <souissiyoussef9@gmail.com>
2024-01-08 18:49:34 +0100

    Custom toast widget

---
+++ therapia_flutter_application/lib/core/widgets/CustomToast.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomToast {
+  static void show(BuildContext context, String message, {bool isSuccess = true}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isSuccess ? Colors.green : Colors.red,
+        duration: Duration(seconds: 15),
+      ),
+    );
+  }
+}
